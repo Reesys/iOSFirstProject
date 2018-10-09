70d205433f8fcc389d04adbeaec05d9ef1876b6f
Reesys <emailjoshuareyes@gmail.com>
2018-10-09 12:20:48 -0600

    Added model group and renamed files.

---
+++ iOSFirstProject/Model/ColorTool.swift
@@ -0,0 +1,15 @@
+//
+//  ColorTool.swift
+//  iOSFirstProject
+//
+//  Created by Reyes, Joshua on 10/9/18.
+//  Copyright © 2018 CTEC. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+public class ColorTool
+{
+    
+}
